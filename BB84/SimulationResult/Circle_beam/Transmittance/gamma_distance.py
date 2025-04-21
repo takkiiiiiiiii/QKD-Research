5b9e1a78abece8512c85991b6568c515a54b999e
Yudai Takihara <yudai2002brazil@gmail.com>
2025-04-21 17:13:01 +0900

    simulate combined transmissivity

--- BB84/SimulationResult/Circle_beam/Transmittance/gamma_distance.py
+++ BB84/SimulationResult/Circle_beam/Transmittance/gamma_distance.py
@@ -5,18 +5,39 @@ import numpy as np
 simulation_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../', 'Model'))
 sys.path.append(simulation_path)
 from circle_beam_transmissivity import transmissivity_etab, satellite_ground_distance, beam_waist, to_decimal_string
-from atmospheric_transmissivity import atmospheric_transmittance
+from atmospheric_transmissivity import transmissivity_etat
 
 
-a = 0.75                # Aparture radius
-G = 6.67430e-11         # Gravitational constant
-M_T = 5.972e24          # Earth's mass
-D_E = 6378e3            # Earth's radius (km)
-h_s = 400e3             # Satellite's altitude
+#=======================================================#
+# eta_t parameters (Atmospheric loss)
+#=======================================================#
+    #=====================#
+    # tau_zen   : Transmission efficiency at zenith
+    # theta_zen : zenith angle
+    #======================#
+# tau_zen = 0.91
+
+
+#=======================================================#
+# eta_b parameters (Pointing loss)
+#=======================================================#
+    #=====================#
+    # a   : Aparture radius
+    # G   : Gravitational constant
+    # M_T : Earth's mass
+    # D_E : Earth's radius (km)
+    # h_s : Satellite's altitude
+    #======================#
+a = 0.75               
+G = 6.67430e-11       
+M_T = 5.972e24      
+D_E = 6378e3       
+h_s = 500e3       
+
 d_o = D_E + h_s
 omega = math.sqrt(G * M_T / d_o**3)
 T = 2 * math.pi / omega
-t = T * 0.0             # 周回時間
+t = T * 0.0             # Time in orbit of the satellite (the time elapsed when the satellite moves from the reference point)
 
 def calculate_theta_deg_from_Rt(R_t):
     cos_theta = (D_E**2 + d_o**2 - R_t**2) / (2 * D_E * d_o)
@@ -31,11 +52,8 @@ def calculate_theta_deg_from_Rt(R_t):
     return theta_p_deg
 
 
-
 def main():
-    displacement = np.arange(0, 3.1, 0.1)
-    r = [a * d for d in displacement]
-
+    r = np.arange(0, 51, 1)
     tau_zen_list = [0.91, 0.85, 0.75, 0.53]
 
     # theta_min = 0
@@ -60,16 +78,24 @@ def main():
     #     plt.plot(displacement, gamma_percent, marker='o', label=f'θ_p={theta_deg:.1f}° (R(t)={R_t/1e3:.1f} km)')
 
     waist = 4
-    R_t = 400e3
+    R_t = 500e3
     theta_deg = calculate_theta_deg_from_Rt(R_t)
     for tau_zen in tau_zen_list:
+        if tau_zen == 0.91:
+            weather = 'Clear sky'
+        elif tau_zen == 0.85:
+            weather = 'Slightly hazy'
+        elif tau_zen == 0.75:
+            weather = 'Noticeably hazy'
+        else:
+            weather = 'Poor visibility'
         eta_b = [transmissivity_etab(a, r_val, waist) for r_val in r]
-        eta_t = atmospheric_transmittance(tau_zen, theta_deg)
+        eta_t = transmissivity_etat(tau_zen, theta_deg)
         gamma_percent = [eta_b_i * eta_t * 100 for eta_b_i in eta_b]
 
-        plt.plot(displacement, gamma_percent, marker='o', label=fr'$\tau_{{zen}}$={tau_zen}')
+        plt.plot(r, gamma_percent, marker='o', label=fr'{weather}')
 
-    plt.xlabel(f"Beam centroid displacement,  r / a (a={a} m)", fontsize=14)
+    plt.xlabel(f"Beam centroid displacement r m", fontsize=14)
     plt.ylabel(r"Combined Transmissivity $\gamma$ [%]", fontsize=14)  # % に変更
     plt.title(fr"Received Transmissivity vs Beam Displacement  ($\theta_\text{{zen}}$={theta_deg:.1f}°, LoS={R_t/1e3:.1f} km)", fontsize=16)
     plt.grid(True)
