a859b6d4dc355465ce6ba3d15b2d687f499f19f8
Yudai Takihara <yudai2002brazil@gmail.com>
2025-04-21 19:15:31 +0900

    simulate the transmissivity for atmospheric loss

--- BB84/SimulationResult/Circle_beam/Atmospheric/transmissivity_t_zenith.py
+++ BB84/SimulationResult/Circle_beam/Atmospheric/transmissivity_t_zenith.py
@@ -4,19 +4,27 @@ import numpy as np
 import os, sys
 simulation_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../', 'Model'))
 sys.path.append(simulation_path)
-from atmospheric_transmissivity import atmospheric_transmittance
+from atmospheric_transmissivity import transmissivity_etat
 
 
 
 def main():
     tau_zen_values = [0.91, 0.85, 0.75, 0.53]
-    angles = np.linspace(0, 60, 100)
+    angles = np.linspace(-60, 60, 100)
 
     plt.figure(figsize=(8, 5))
 
     for tau_zen in tau_zen_values:
-        transmissions = [atmospheric_transmittance(tau_zen, angle) for angle in angles]
-        plt.plot(angles, transmissions, label=fr"$\tau_{{zen}} = {tau_zen}$")
+        if tau_zen == 0.91:
+            weather = 'Clear sky'
+        elif tau_zen == 0.85:
+            weather = 'Slightly hazy'
+        elif tau_zen == 0.75:
+            weather = 'Noticeably hazy'
+        else:
+            weather = 'Poor visibility'
+        transmissions = [transmissivity_etat(tau_zen, angle) for angle in angles]
+        plt.plot(angles, transmissions, label=fr"{weather}")
 
     plt.xlabel("Zenith Angle (°)", fontsize=14)
     plt.ylabel("Atmospheric Transmittance", fontsize=14)
