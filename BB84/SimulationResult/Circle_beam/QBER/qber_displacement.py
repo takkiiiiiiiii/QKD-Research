ef0843ec962e6c53d995d2c55dde82151ff16624
Yudai Takihara <yudai2002brazil@gmail.com>
2025-04-27 16:17:32 +0900

    WIP:simulate the qber each displacements

--- BB84/SimulationResult/Circle_beam/QBER/qber_displacement.py
+++ BB84/SimulationResult/Circle_beam/QBER/qber_displacement.py
@@ -19,7 +19,6 @@ from qber import qber_loss
     #======================#
 tau_zen = 0.91
 
-
 #=======================================================#
 # eta_b parameters (Pointing)
 #=======================================================#
@@ -46,22 +45,20 @@ t = T * 0.0             # Time in orbit of the satellite (the time elapsed when 
 # QBER parameters
 #=======================================================#
     #=====================#
-    # n_s   : average numher of photon
-    # n_D   : dark-current-equivalent averrage photon number
-    # eta   : APD (single-photon avalanche photodiode) detection efficiency
-    # n_B   : background photons per polarization
-    # n_N   : the average number of noise photon reaching each detector
-    # gamma : the fraction of transmmited photon
+    # n_s   : average numher of photon from Alice
+    # e_0   : the error rate of the background
+    # Y_0   : the background rate which includes the detector dark count and other background contributions
+    # e_dec : the probability that a photon hits the erroneous detector
     #======================#
 n_s = 10e8
-n_D = math.pow(10, -6)
-eta = 0.5  
-n_B = math.pow(10, -3)
-n_N = n_B/2 + n_D
+e_0 = 0.5
+Y_0 = 10e-5
+e_dec = 0.01
+
 
 
 def main():
-    r = np.arange(0, 51, 1) 
+    r = np.arange(0, 7, 0.5) 
 
     theta_min = math.radians(0)
     theta_max = math.radians(10)
@@ -86,7 +83,7 @@ def main():
         gamma = [eta_b_i * eta_t for eta_b_i in eta_b]
 
         # QBER = qber(gamma)
-        qber_values = [qber_loss(eta, n_s, n_N, gamma_i) for gamma_i in gamma]
+        qber_values = [qber_loss(gamma_i) * 100 for gamma_i in gamma]
 
         # QBERの値を表示
         print(f"\n--- θ_p = {theta_deg:.2f}° (R={R_t/1e3:.2f} km, eta_t={eta_t:.4f}) ---")
@@ -96,12 +93,15 @@ def main():
         # グラフ描画
         plt.plot(r, qber_values, marker='o', label=fr'$\theta_{{zen}}$={theta_deg:.1f}° (R={R_t/1e3:.1f} km)')
 
-    plt.xlabel(f"Beam centroid displacement r (m), (a = {a}m)", fontsize=14)
-    plt.ylabel(r"QBER (Quantum Bit Error Rate)", fontsize=14)
-    plt.title(fr"QBER vs Beam Displacement  ($\tau_{{zen}}$={tau_zen})", fontsize=16)
+    plt.xlabel(f"Beam centroid displacement r (m), (a = {a}m)", fontsize=20)
+    plt.ylabel(r"QBER (Quantum Bit Error Rate) [%]", fontsize=20)
+    plt.title(fr"QBER vs Beam Displacement  (Clear Sky)", fontsize=20)
     plt.grid(True)
     plt.legend(fontsize=12)
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
     plt.tight_layout()
+
 
     output_path = os.path.join(os.path.dirname(__file__), "qber_vs_r_with_tauzen.png")
     plt.savefig(output_path)
