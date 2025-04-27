d8af18dca427cab7fea505d856086bb0c8b03dea
Yudai Takihara <yudai2002brazil@gmail.com>
2025-04-27 19:31:23 +0900

    simulate transmittance for pointing

--- BB84/SimulationResult/Circle_beam/Pointing/transmissivity_b_displacement.py
+++ BB84/SimulationResult/Circle_beam/Pointing/transmissivity_b_displacement.py
@@ -4,7 +4,7 @@ import os, sys
 import numpy as np
 simulation_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../', 'Model'))
 sys.path.append(simulation_path)
-from circle_beam_transmissivity import transmissivity_etab, beam_waist, to_decimal_string
+from circle_beam_transmissivity import transmissivity_etab, beam_waist, satellite_ground_distance
 
 
 a = 0.75              # Aparture radius
@@ -23,9 +23,10 @@ def main():
     print("===============================")
     print(f'Aperture radius (Receiver): {a} m')
 
+
     # θ_pの範囲（ラジアン）
     theta_min = math.radians(0)
-    theta_max = math.radians(60)
+    theta_max = math.radians(10)
     theta_list = np.linspace(theta_min, theta_max, 5)  # プロットする角度数（5本）
 
     plt.figure(figsize=(9, 6))
@@ -33,6 +34,8 @@ def main():
     for theta_rad in theta_list:
         # 角度→時間へ
         t = theta_rad / omega
+        # LoS distance
+        R_t = satellite_ground_distance(h_s, t)
         # ビームウエスト計算
         waist = beam_waist(h_s, t)
         # 各rでの eta_b 計算
@@ -41,7 +44,7 @@ def main():
         theta_deg = math.degrees(theta_rad)
 
         # プロット
-        plt.plot(r, eta_b, marker='o', label=fr'Zenith angle($\theta_{{zen}}$)={theta_deg:.1f}°')
+        plt.plot(r, eta_b, marker='o', label=fr'$\theta_{{zen}}$={theta_deg:.1f}° (R={R_t/1e3:.1f} km)')
 
     print("===============================\n")
 
