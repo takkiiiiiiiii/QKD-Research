9bf70d35cc3019417b3dde8a0366496007b9a496
Yudai Takihara <yudai2002brazil@gmail.com>
2025-04-30 18:03:53 +0900

    modify the parameter

--- BB84/Model/new_qber.py
+++ BB84/Model/new_qber.py
@@ -6,14 +6,6 @@ from qber import qber_loss
 
 
 
-a = 0.75      # Aperture of radius (Receiver radis in meters)
-ratios = np.arange(0, 3.1, 0.1)
-r0 = [r * a for r in ratios]
-mag_w1 = [0.2, 1.0, 1.8]
-mag_w2 = [0.1, 0.9, 1.7]
-chi = [math.pi/3, math.pi/4, math.pi/5]
-chi_show = [3, 4, 5]
-
 def qner_new_infinite():
     def integrand(gamma):
         return fading_loss(gamma) * qber_loss(gamma)
