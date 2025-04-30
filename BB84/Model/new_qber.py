cb357972aae59b8f59bba5aa82898e3ad9f38309
Yudai Takihara <yudai2002brazil@gmail.com>
2025-04-30 17:50:34 +0900

    moke the new QBER function

--- BB84/Model/new_qber.py
+++ BB84/Model/new_qber.py
@@ -14,19 +14,18 @@ mag_w2 = [0.1, 0.9, 1.7]
 chi = [math.pi/3, math.pi/4, math.pi/5]
 chi_show = [3, 4, 5]
 
-
 def qner_new_infinite():
     def integrand(gamma):
         return fading_loss(gamma) * qber_loss(gamma)
-
     result, _ = quad(integrand, 0, np.inf, limit=100, epsabs=1e-9, epsrel=1e-9)
     return result
 
 
 def main():
-    gamma = 0.5
-    qber = qner_new_infinite(gamma)
+    qber = qner_new_infinite()
     print(f'QBER: {qber}')
 
 if __name__ == "__main__":
     main()
+
+
