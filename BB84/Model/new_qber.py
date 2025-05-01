7b4655d3d617baa0187e2676273d4e308c0c30fd
Yudai Takihara <yudai2002brazil@gmail.com>
2025-05-01 17:16:09 +0900

    WIP:make the model for new_qber

--- BB84/Model/new_qber.py
+++ BB84/Model/new_qber.py
@@ -9,7 +9,7 @@ from qber import qber_loss
 def qner_new_infinite():
     def integrand(gamma):
         return fading_loss(gamma) * qber_loss(gamma)
-    result, _ = quad(integrand, 0, np.inf, limit=100, epsabs=1e-9, epsrel=1e-9)
+    result, _ = quad(integrand, 0, 1, limit=100, epsabs=1e-9, epsrel=1e-9)
     return result
 
 
